a8dbe0d76d2ac67aeeb976388a7e644d275ff1d3
Katherine Love <270010932@bun2-211-05.local>
2019-08-27 15:22:54 +1200

    stuff

---
+++ js/script.js
@@ -0,0 +1,43 @@
+const id = $('#heading');
+console.log(id);
+
+
+const listItems = $('.listItems');
+console.log(listItems);
+
+const element = $('p');
+console.log(element);
+
+$('#heading').click(function() {
+  console.log($(this).text());
+  // $('p').hide();
+})
+
+$('.listItems').click(function() {
+  $(this).hide();
+  $('p').show();
+});
+
+$('#blueBtn').click(function() {
+  $('.blue').toggle();
+})
+
+$('#greyBtn').click(function() {
+  $('.grey').toggle();
+})
+
+$('#greenBtn').click(function() {
+  $('.green').toggle();
+})
+
+$('#hideBtn').click(function() {
+  $('.blue').hide();
+  $('.grey').hide();
+  $('.green').hide();
+})
+
+$('#showBtn').click(function() {
+  $('.blue').show();
+  $('.grey').show();
+  $('.green').show();
+})
